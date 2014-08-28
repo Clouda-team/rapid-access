5d3f531e66ec8dd64e75d556dd492973f4f8667c
kyriosli <lz3259@gmail.com>
2014-08-28 21:31:59 +0800

    add memcache test case

--- test/cache.js
+++ test/cache.js
@@ -44,3 +44,63 @@ describe('cache', function () {
     })
 
 });
+
+describe('memcache', function () {
+    this.timeout(5000);
+    var db;
+    it('wait for config ready', function (next) {
+        rapid.resource.watch('cachedDb', function ($db) {
+            db = $db;
+            next();
+        });
+    });
+    it('set', function (next) {
+        db.set('test.123', {name: 'cachedDB.set', gid: 234}).then(function () {
+            next();
+        }).done();
+    });
+    it('get', function (next) {
+        db.get('test.123').then(function (ret) {
+            assert(ret.name === 'cachedDB.set');
+            assert(ret.gid === 234);
+            next();
+        }).done();
+    });
+    it('find', function (next) {
+        db.find('test', {id: 123}).then(function (ret) {
+            ret = ret[0];
+            assert(ret.name === 'cachedDB.set');
+            assert(ret.gid === 234);
+            next();
+        }).done();
+    });
+    it('update internally', function (next) {
+        db.update('test', {gid: 345}, {
+            where: {id: 123}
+        }).then(function () {
+            return db.get('test.123');
+        }).then(function (ret) {
+            assert(ret.name === 'cachedDB.set');
+            assert(ret.gid === 234);
+            next();
+        }).done();
+    });
+    it('cached find', function (next) {
+        db.find('test', {id: 123}).then(function (ret) {
+            ret = ret[0];
+            assert(ret.name === 'cachedDB.set');
+            assert(ret.gid === 234);
+            next();
+        }).done();
+    });
+    it('find with noCache', function (next) {
+        db.find('test', {id: 123}, {
+            noCache: true
+        }).then(function (ret) {
+            ret = ret[0];
+            assert(ret.name === 'cachedDB.set');
+            assert(ret.gid === 345);
+            next();
+        }).done();
+    });
+});
--- test/conf/config.js
+++ test/conf/config.js
@@ -1,5 +1,7 @@
 rapid.config.define('dal', {
     appdb: 'mysql://root:root@localhost/test',
     httpServer: 'http://root:root@127.0.0.5:8032/',
-    cache: 'cache:///cache'
+    cache: 'cache:///cache',
+    cachedDb: 'mysql://root:root@localhost/test?cache=' +
+        encodeURIComponent('memcache://3c0cd9292dbf11e4:Good_Job@127.0.0.1/')
 });
